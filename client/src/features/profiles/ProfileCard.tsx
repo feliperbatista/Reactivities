5bb7d3cbbbce9b64738ef2f94b10e869262acd7c
Felipe Batista <feliperodriguesbatista@outlook.com>
2026-03-08 15:52:35 -0300

    feat: add following and followers

--- client/src/features/profiles/ProfileCard.tsx
+++ client/src/features/profiles/ProfileCard.tsx
@@ -15,11 +15,10 @@ type Props = {
 };
 
 export default function ProfileCard({ profile }: Props) {
-  const following = false;
   return (
     <Link to={`/profiles/${profile.id}`} style={{ textDecoration: 'none' }}>
       <Card
-        sx={{ borderRadius: 3, p: 3, maxWidth: 300, textDecoration: 'none' }}
+        sx={{ borderRadius: 3, p: 3, maxWidth: 250, textDecoration: 'none' }}
         elevation={4}
       >
         <CardMedia
@@ -43,7 +42,7 @@ export default function ProfileCard({ profile }: Props) {
                 {profile.bio}
               </Typography>
             )}
-            {following && (
+            {profile.following && (
               <Chip
                 size='small'
                 label='Following'
@@ -62,7 +61,7 @@ export default function ProfileCard({ profile }: Props) {
           }}
         >
           <Person />
-          <Typography sx={{ ml: 1 }}>20 followers</Typography>
+          <Typography sx={{ ml: 1 }}>{profile.followersCount} followers</Typography>
         </Box>
       </Card>
     </Link>
